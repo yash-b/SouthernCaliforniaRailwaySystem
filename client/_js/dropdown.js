79a63d99644f15e5cfa142ebaa987052f2afeec4
Yash <yash_b@csu.fullerton.edu>
2020-12-07 19:39:39 -0800

    adding server files

---
+++ client/_js/dropdown.js
@@ -0,0 +1,15 @@
+// $(document).ready(function () {
+// 	$('ul#nav li.drop2').mouseover(function () {
+// 		$('ul#nav ul.dropp2').css('display','block');
+// 	});
+// 	$('ul#nav ul.dropp2').mouseout(function(){
+// 		$('ul#nav ul.dropp2').css('display','none')  
+// 	});
+// 	$('ul#nav li.drop1').mouseover(function () {
+// 		$('ul#nav ul.dropp1').css('display','block');
+// 	});
+// 	$('ul#nav ul.dropp1').mouseout(function(){
+// 		$('ul#nav ul.dropp1').css('display','none')
+		            
+// 	});
+// });
